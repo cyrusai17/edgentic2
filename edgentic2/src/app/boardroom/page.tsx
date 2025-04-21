9099b394d9e5beb56725a87aa4775b95833f0416
Eric Greenberg <ericgreenberg@Doloress-iMac.local>
2025-04-21 14:49:36 -0400

    Fix Explore Programs button link on Boardroom page

--- edgentic2/src/app/boardroom/page.tsx
+++ edgentic2/src/app/boardroom/page.tsx
@@ -61,7 +61,7 @@ export default function BoardroomPage() {
                   Join The Boardroom
                 </motion.button>
               </Link>
-              <Link href="/programs">
+              <Link href="/catalog">
                 <motion.button
                   whileHover={{ scale: 1.05 }}
                   whileTap={{ scale: 0.95 }}
